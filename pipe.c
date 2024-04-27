b4b01dd976f9bb80a9fa3e2088a683722887af9f
Anika Bala <anikabalak1@gmail.com>
2024-04-26 18:45:47 -0700

    comments for readability

--- pipe.c
+++ pipe.c
@@ -5,145 +5,64 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+	// first process - take in std input, output to write end of pipe
+	// middle processes - take in from read end, output to write end of pipe
+	// last process - take in from read end, output to standard output
 
-	// //first process - take in std input, output to write end of pipe
-	// //middle processes - take in from read end, output to write end of pipe
-	// //last process - take in from read end, output to standard output
+	// for every p arguments, there are p-1 pipes
+	// for every p arguments, there are p forks!!!
 
-	// //for every p processes, there is p-1 pipes
-	// //for i = 1, i < argc - 1, i++
-
-
-
-	// //iterate through the arguments
-	// // ./pipe ls cat wc -- 2 pipes
-
-	// for(int i = 1; i <= argc - 1; i++){
-	// 	printf("About to fork\n");
-	// 	int child_code = fork(); //fork
-	
-
-	// 	if(child_code == 0 && i == 1){
-	// 		printf("We are in the child process!\n");
-	// 		printf("Argument # %d\n", i);
-	// 		dup2(write_end, STDOUT_FILENO); //redirect output to the write end of pipe
-	// 		close(read_end);
-	// 		close(write_end);
-	// 		execlp(argv[i], argv[i], NULL);
-	// 		perror("exec");
-    //         	_exit(1); //replace w eval
-	// 		exit(0);//dont know if i need this
-
-	// 	} else if(child_code == 0){
-	// 		dup2(write_end, STDOUT_FILENO);
-	// 		dup2(read_end, STDIN_FILENO);
-	// 		close(read_end);
-	// 		close(write_end);
-	// 		execlp(argv[i], argv[i], NULL);
-	// 		perror("exec");
-	// 			_exit(1); //replace w eval
-	// 		exit(0);	
-	// 	} else if(child_code > 0 && i = argc - 1){
-	// 		printf("We are in the parent process!\n");
-	// 		printf("Let's let the kid do the work:\n");
-	// 		dup2(read_end, STDIN_FILENO);
-	// 		// dup2(STDOUT_FILENO, STDOUT_FILENO); //this is redirecting 0 (input) to the read end of the pipe
-	// 		close(write_end);
-	// 		int status = 0;
-	// 		//add some wifexited type shit
-	// 		waitpid(child_code, &status, 0);
-	// 		close(read_end);// Close the read end of the pipe in the parent process
-	// 	} else if(child_code > 0){
-
-	// 	}
-	// 	else {
-	// 		printf("Child process creation error!");
-	// 	}
-	// }
-	
-
-	// printf("All done!\n");
-	// return 0;
-
-
-	//special no args exit
-	// if (argc < 2) {
-    //     printf("No arguments provided.\n");
-    //     return EINVAL;
-    // } 
-
-// 	//case if only one argument -- reduces complexity by just directly running execlp
-// 	if(argc == 2){
-// 		execlp(argv[1], argv[1], NULL);
-// 		exit(0);
-// 	}
-
-// 	//define array pipefd
-// 	int pipefd[2];
-
-// 	int read_end = pipefd[0];
-// 	int write_end = pipefd[1];
-
-// 	int i;
-//     for(i = 1; i < argc-1; i++) {
-        
-//         pipe(pipefd);
-
-//         if (!fork()) { 
-//             dup2(write_end, 1); 
-//             execlp(argv[i], argv[i], NULL);
-//             perror("exec");
-//             abort();//eval
-//         }
-
-//         dup2(read_end, 0);
-//         close(read_end);
-		
-//     }
-
-//     execlp(argv[i], argv[i], NULL);
-//     perror("exec");
-//     abort();//eval 
-// }
 
 int main (int argc, char* argv[]) {
 	
+	//special no argument case, exit immediately
 	if (argc < 2) {
         printf("No arguments provided.\n");
 		errno = EINVAL;
         exit(errno);
     } 
 
+	//create int i to track arguments 
     int i;
 
+	//iterate through arguments 
     for(i = 1; i < argc - 1; i++) {
-        int pipefd[2];
-        pipe(pipefd);
+        int pipefd[2]; //create pipefd array
+        pipe(pipefd); //create pipe!
 
-        if (!fork()) {
-            dup2(pipefd[1], STDOUT_FILENO); // remap output back to parent
-			close(pipefd[0]);
-			close(pipefd[1]);
-            execlp(argv[i], argv[i], NULL);
-            perror("exec");
+        if (!fork()) { //if fork() does not return a negative number (aka it works)
+            dup2(pipefd[1], STDOUT_FILENO); // redirect std output to the 'write' end of the pipe
+			close(pipefd[0]); //close the read end
+			close(pipefd[1]); //close the write end
+            execlp(argv[i], argv[i], NULL); //execute the first passed-in program
+
+			//if execlp returns, there was an execution error, exit
+            perror("exec"); 
 			errno = ENOEXEC;
-            exit(errno);//replace w e val
+            exit(errno); 
         }
 
-        // remap output from previous child to input
+        // redirect std input to the 'read' end of the pipe
         dup2(pipefd[0], STDIN_FILENO);
-		close(pipefd[0]);
-        close(pipefd[1]);
+		close(pipefd[0]); //close the read end
+        close(pipefd[1]); //close the write end
 
+		
+		//status variable to keep track
 		int status;
-        if (waitpid(-1, &status, 0) == -1) {
+
+		//call waitpid to wait for child processes (no orphans), else exit
+        if (waitpid(-1, &status, 0) == -1) { 
             perror("waitpid");
 			errno = ECHILD;
-            exit(errno);//replace w e val
+            exit(errno);
         }
     }
 
+	//outside loop, to execute the final argument!
     execlp(argv[i], argv[i], NULL);
+
+	//if cannot execute, exit 
     perror("exec");
 	errno = ENOEXEC;
     exit(errno);
